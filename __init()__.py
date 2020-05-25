4e24bd6e9a72978676d1bbb1bd8699a57c8c040c
Andrea Fecchio <48335273+fecchioandrea@users.noreply.github.com>
2020-05-25 18:00:45 +0200

    Update __init()__.py

--- __init()__.py
+++ __init()__.py
@@ -9,7 +9,7 @@ se assente allora viene riempito il payload indicato direttamente nell'url.
 La prova (per il POST) utilizza una Web App su Virtual Machine, indirizzo:
 http://192.168.56.101/index.php/, in cui ad un payload viene sostituito
 di volta in volta un diverso codice di intrusione, tra quelli nel file
-'Auth_Bypass.txt' nella repository Github 'PayloadsAllTheThings'.
+'Auth_Bypass.txt', nella repository: https://github.com/swisskyrepo/PayloadsAllTheThings.
 
 Nel caso particolare, usiamo 'Administrator' come 'uname',
 mentre 'psw' corrisponde al campo il cui valore va ricercato.
